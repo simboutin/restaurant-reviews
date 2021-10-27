800b5c9d06ee75a31adc85344453b55d38c2bacd
Simon Boutin <simboutin@gmail.com>
2021-10-27 12:15:49 +0200

    add review destroy action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :find_restaurant
+  before_action :find_restaurant, except: :destroy
 
   def new
     @review = Review.new
@@ -13,6 +13,12 @@ class ReviewsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # [...]
   resources :restaurants do
     # collection
     # /restaurants/...
@@ -9,14 +12,13 @@ Rails.application.routes.draw do
     # member
     # /restaurants/:id/...
     member do
+      # get 'restaurants/:id/reviews/new' ...
       get :chef
     end
 
     resources :reviews, only: %i[new create]
   end
 
-  # get 'restaurants/:id/reviews/new'
-  # get 'restaurants', to: 'restaurants#index'
-  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reviews, only: :destroy
+  # delete 'reviews/:id' ...
 end
