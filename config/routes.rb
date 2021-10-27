42bff268eaca6fb23aefcfd2cf1d40dccc15865a
Simon Boutin <simboutin@gmail.com>
2021-10-27 07:30:21 +0200

    create restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
