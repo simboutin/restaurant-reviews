e8aaab483bae445a5175c16715f12ef88dfeddcb
Simon Boutin <simboutin@gmail.com>
2021-10-27 08:36:04 +0200

    add reviews and new/create actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -33,7 +33,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
 
     if @restaurant.save
-      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
     member do
       get :chef
     end
+
+    resources :reviews, only: %i[new create]
   end
+
+  # get 'restaurants/:id/reviews/new'
   # get 'restaurants', to: 'restaurants#index'
   # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
